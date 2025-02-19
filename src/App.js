e8fdc1f476bd1c859f45c30620a0c6ec0d81b62b
PoChuanSu <peterpeter1010@live.com>
2025-02-19 19:31:12 +1100

    onclick function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ const messages = [
 export default function App() {
     const step = 1;
 
+    function handlePrevious() {
+        alert("Previous");
+    }
+    function handleNext() {
+        alert("Next");
+    }
+
     return (
         <>
             <div className="steps">
@@ -22,11 +29,13 @@ export default function App() {
                 <div className="buttons">
                     <button
                         style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                        onClick={handlePrevious}
                     >
                         Previous
                     </button>
                     <button
                         style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                        onClick={handleNext}
                     >
                         Next
                     </button>
